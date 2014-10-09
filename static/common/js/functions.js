276f576b2daa644f86f988bd478176e06cd79eda
ΚΣΣGΔΠ ΒRΘΨΠ <brown.keegan@gmail.com>
2014-10-08 19:09:18 -0500

    gosh

---
+++ static/common/js/functions.js
@@ -0,0 +1,17 @@
+// Browser detection for when you get desparate. A measure of last resort.
+// http://rog.ie/post/9089341529/html5boilerplatejs
+
+// var b = document.documentElement;
+// b.setAttribute('data-useragent',  navigator.userAgent);
+// b.setAttribute('data-platform', navigator.platform);
+
+// sample CSS: html[data-useragent*='Chrome/13.0'] { ... }
+
+
+// remap jQuery to $
+(function($){
+
+	$('.no-js').removeClass('no-js');
+	
+
+})(window.jQuery);
